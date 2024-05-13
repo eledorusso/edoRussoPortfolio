013b2ba9b21941d08311530bc7fa019fbec085ec
eledorusso <robinmoneycompany@gmail.com>
2024-05-13 21:35:19 +0200

    fixed width overflow

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import "./App.css";
 import Page2 from "./components/Page2/Page2.jsx";
 import Page3 from "./components/Page3/Page3.jsx";
 import Page4 from "./components/Page4/Page4.jsx";
-import { isMobile, isBrowser } from "react-device-detect";
+// import { isMobile, isBrowser } from "react-device-detect";
 
 function App() {
   const [smoke, setSmoke] = useState(false);
@@ -59,65 +59,66 @@ function App() {
       });
     }
   };
-  if (isBrowser) {
-    return (
-      <>
-        <div className="mainContainer">
-          <h1 className="mainTitle">
-            <span className="hidden md:block text-4xl">HELLO, </span>
-            <span className="imEdoRusso">
-              I'M <span style={{ color: "black" }}>EDO RUSSO</span>
-            </span>
-            <span className="text-4xl">& THIS IS MY PORTFOLIO</span>
-          </h1>
-          {/* BACKGROUND IMAGE */}
-          <img
-            className="absolute h-full right-0 object-cover z-10"
-            src="/imgs/bg-img-1.jpg"
-          />
-          {/* This can not be inside the yellow rectangle because the recangle changes it's opacity */}
+  // if (isBrowser) {
+  return (
+    <>
+      <div className="mainContainer">
+        <h1 className="mainTitle">
+          <span className="hidden md:block text-4xl">HELLO, </span>
+          <span className="imEdoRusso">
+            I'M <span style={{ color: "black" }}>EDO RUSSO</span>
+          </span>
+          <span className="text-4xl">& THIS IS MY PORTFOLIO</span>
+        </h1>
+        {/* BACKGROUND IMAGE */}
+        <img
+          className="absolute h-full right-0 object-cover z-10"
+          src="/imgs/bg-img-1.jpg"
+        />
+        {/* This can not be inside the yellow rectangle because the recangle changes it's opacity */}
 
-          <p
-            className={
-              smoke ? "scrollToSeeMyProjectsSmoke" : "scrollToSeeMyProjects"
-            }
-          >
-            SCROLL TO <br />
-            <span style={{ textDecoration: "underline" }}>SEE MY PROJECTS</span>
-          </p>
-          <div className="yellowHorizontalRectangle" />
-
-          {/* Scroll Button POSITION STATIC */}
-          <button
-            onClick={scrollTo}
-            className={!afterSmoke ? "scrollIcon" : "scrollIconJumping"}
-          >
-            <img
-              src={
-                scrollArrow === "down"
-                  ? "/imgs/scroll-icon.png"
-                  : "/imgs/scroll-icon-up.jpg"
-              }
-            />
-          </button>
-        </div>
-
-        <Page2 />
-        <Page3 />
-        <Page4 />
-      </>
-    );
-  } else if (isMobile) {
-    return (
-      <div className="w-screen h-screen flex bg-black text-white items-center justify-center">
-        <p style={{fontSize: '1.5rem'}}>
-          THIS SITE HASN'T<br/> BEEN OPTIMIZED
-          <br /> FOR MOBILE YET.<br/><br/>PLEASE USE BROWSER.
-
+        <p
+          className={
+            smoke ? "scrollToSeeMyProjectsSmoke" : "scrollToSeeMyProjects"
+          }
+        >
+          SCROLL TO <br />
+          <span style={{ textDecoration: "underline" }}>SEE MY PROJECTS</span>
         </p>
+        <div className="yellowHorizontalRectangle" />
+
+        {/* Scroll Button POSITION STATIC */}
+        <button
+          onClick={scrollTo}
+          className={!afterSmoke ? "scrollIcon" : "scrollIconJumping"}
+        >
+          <img
+            src={
+              scrollArrow === "down"
+                ? "/imgs/scroll-icon.png"
+                : "/imgs/scroll-icon-up.jpg"
+            }
+          />
+        </button>
       </div>
-    );
-  }
+
+      <Page2 />
+      <Page3 />
+      <Page4 />
+    </>
+  );
+  // }
+  // else if (isMobile) {
+  //   return (
+  //     <div className="w-screen h-screen flex bg-black text-white items-center justify-center">
+  //       <p style={{fontSize: '1.5rem'}}>
+  //         THIS SITE HASN'T<br/> BEEN OPTIMIZED
+  //         <br /> FOR MOBILE YET.<br/><br/>PLEASE USE BROWSER.
+
+  //       </p>
+  //     </div>
+  //   );
+  // }
 }
 
 export default App;
